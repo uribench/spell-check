e804c7103e7ab7cdf9594165c9bf28f31e613f0d
uribench <uribench@gmail.com>
2020-07-11 04:19:32 +0000

    Change initial env value for testing

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -5,4 +5,4 @@ def set_test_env():
     """Set test environment variables"""
     os.environ['SOURCE_XML_FILES'] = './tests/test_fixture.xml'
     os.environ['SOURCE_NODES'] = 'value, label, description, name'
-    os.environ['OUTPUT_FILE_NAME'] = 'None or any text here will redirect output to sys.stdout'
+    os.environ['OUTPUT_FILE_NAME'] = 'Any text here will redirect output to sys.stdout'
